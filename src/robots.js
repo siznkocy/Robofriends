b153a25d67c6c59fc02adead423ca4aa065e5c7a
Sizwe <siznkocie@outlook.com>
2021-07-01 06:37:18 +0200

    initial commit"

---
+++ src/robots.js
@@ -0,0 +1,122 @@
+export const robots = [
+  {
+    id:1,
+    name:'Rio Ford',
+    username:'Cik',
+    email:'rioford@nay.co.za'
+  },
+  {
+    id:2,
+    name:'Naith McColler',
+    username:'Nemasis',
+    email:'nemasis@vok.om'
+  },
+  {
+    id:3,
+    name:'Justin Bieber',
+    username:'justcause',
+    email:'justcause@never.kom'
+  },
+  {
+    id:4,
+    name:'Anthor Rio',
+    username:'meandyou',
+    email:'THEliving@life.uk'
+  },
+  {
+    id:5,
+    name:'James McGEE',
+    username:'Jamesbond',
+    email:'satannate@thru.us'
+  },
+  {
+    id:6,
+    name:'Senzo Jesus',
+    username:'Mabhena',
+    email:'SJesus@oka.za'
+  },
+  {
+    id:7,
+    name:'Justin Bieber',
+    username:'justcause',
+    email:'justcause@never.gov'
+  },
+  {
+    id:8,
+    name:'Anthor Rio',
+    username:'meandyou',
+    email:'THEliving@life.com'
+  },
+  {
+    id:9,
+    name:'James McGEE',
+    username:'Jamesbond',
+    email:'satannate@thru.com'
+  },
+  {
+    id:10,
+    name:'Justin Bieber',
+    username:'justcause',
+    email:'justcause@never.com'
+  },
+  {
+    id: 11,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 12,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 13,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 14,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 15,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 16,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 17,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 18,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 19,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 20,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+] 
